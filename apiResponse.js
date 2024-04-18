16369ea4140506986fdd364d916dd7739aa4b612
Duke242 <innominate3301@proton.me>
2024-04-18 19:51:26 -0400

    clean up

--- apiResponse.js
+++ apiResponse.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request) => {
   if (request.type === "apiResponse") {
     const responseElement = document.getElementById("api-response")
     responseElement.textContent = JSON.stringify(request.data, null, 2)
